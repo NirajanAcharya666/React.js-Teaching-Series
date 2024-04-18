429d52f79790f052dbd421ddddd704cb202ede25
Nirajan Acharya <87110903+NirajanAcharya666@users.noreply.github.com>
2024-04-18 15:18:08 +0000

    new commit

---
+++ Week 1/ES6 refresher/app.js
@@ -0,0 +1,24 @@
+//let and const
+
+
+const myname = 'nirajan';
+
+console.log(myname);
+
+//myname = 'nirajan'; //error
+
+//arrow function 
+
+/* function sayName(name){
+    console.log(name);
+}
+
+sayName('Happy Goal');
+*/
+
+let sayName = name =>
+{
+    return name;
+}
+console.log(sayName('Happy Goal'));
+
---
+++ roadmap.js
@@ -0,0 +1,56 @@
+
+
+
+/* HTML , CSS (flex box, margin, padding), JS (DOM, events, functions) 
+
+advantage : 
+
+1. Easy to learn
+
+2. Fast to develop
+
+3. Good for small projects
+
+4. Good for prototyping
+
+5. Good for quick changes
+
+6. single page applications
+
+7. Good for SEO
+
+8. Good for mobile apps
+
+9. Good for web apps
+
+10. Good for cross-platform apps
+
+
+*/
+
+// Path: roadmap.js
+/* 
+1. HTML 
+2. CSS
+3. JS - X (javascript extended version) (DOM, function, events, classes, modules,
+     promises, async/await, fetch, arrow functions, template literals, destructuring,
+      spread operator, rest operator, default parameters, let, const, var, map, filter, 
+      reduce, find, includes, some, every, sort, reverse, slice, splice, concat, split,
+       join, indexOf, lastIndexOf, includes, push, pop, shift, unshift, forEach, map, 
+       filter, reduce, find, findIndex, some, every, sort, reverse, slice, splice, concat,
+        split, join, indexOf, lastIndexOf, includes, push, pop, shift, unshift, forEach, 
+        map, filter, reduce, find, findIndex, some, every, sort, reverse, slice, splice, 
+        concat, split, join, indexOf, lastIndexOf, includes, push, pop, shift, unshift, 
+        forEach, map, filter, reduce, find, findIndex, some, every, sort, reverse, slice, 
+        splice, concat, split, join, indexOf, lastIndexOf, includes, push, pop, shift, unshift, 
+        forEach, map, filter, reduce, find, findIndex, some, every, sort, reverse, slice, splice, concat,
+         split, join, indexOf, lastIndexOf, includes, push, pop, shift, unshift, forEach, map, filter, reduce,
+          find, findIndex, some, every, sort, reverse, slice, splice, concat, split, join, indexOf, lastIndexOf, includes, push, pop, shift, unshift, forEach, map, filter, reduce, find, findIndex, some, every, sort, reverse, slice, splice, concat, split, join, indexOf, lastIndexOf, includes, push, pop, shift, unshift, forEach, map, filter, reduce, find, findIndex, some, every, sort, reverse, slice, splice, concat, split, join, indexOf, lastIndexOf, includes, push, pop, shift, unshift, forEach, map, filter, reduce, find, findIndex, some, every, sort, reverse, slice, splice, concat, split, join, indexOf, lastIndexOf, includes, push, pop, shift, unshift, forEach, map, filter, reduce, find, findIndex, some, every, sort, reverse, slice, splice, concat, split, join, indexOf, lastIndexOf, includes, push, pop, shift)
+
+4. React.js
+*/
+
+
+
+
+
